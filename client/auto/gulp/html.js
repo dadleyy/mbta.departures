93d822f9ca63a68524071ae4ec94acf453d55945
Danny Hadley <dadleyy@gmail.com>
2016-07-08 18:05:52 -0400

    adding gulp compilation of index.jade file to output directory

--- client/auto/gulp/html.js
+++ client/auto/gulp/html.js
@@ -1,11 +1,23 @@
 "use strict";
 
+const path = require("path");
+const del  = require("del");
+const pug  = require("gulp-pug");
+
 module.exports = function(gulp) {
 
+  let base = path.join(__dirname, "../../");
+
   gulp.task("clean:html", function() {
+    return del([
+      path.join(base, "dist/index.html")
+    ]);
   });
 
   gulp.task("html", ["clean:html"], function() {
+    return gulp.src(path.join(base, "src/html/index.jade"))
+      .pipe(pug({pretty: true}))
+      .pipe(gulp.dest(path.join(base, "dist")));
   });
 
 };
